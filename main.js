76800f40f76f2f09dffdb8762046673c80f1a0b7
DRPHz <144113616+DRPHz@users.noreply.github.com>
2023-10-11 12:20:06 +0200

    Changed positions
    navigation bar is now centered, navbar has a bit more space aswell as leaving the main content in the center after closing the sidebar

--- main.js
+++ main.js
@@ -6,15 +6,6 @@ function openNav() {
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main-content").style.marginLeft = "0";
+  document.getElementById("main-content").style.marginLeft = "auto";
   document.getElementById("main-content").style.opacity = "1";
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-  let mainContent = document.getElementById("main-content");
-  mainContent.style.opacity = "0";
-  setTimeout(function () {
-    mainContent.style.opacity = "1";
-    mainContent.style.transform = "translateY(0)";
-  }, 100);
-});
